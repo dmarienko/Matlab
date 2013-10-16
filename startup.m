8aeeb888965c4e0aab9e30cf9eeda839b394f7b6
dima <dmitry.ema@gmail.com>
2013-10-16 21:11:04 +0300

    refactoring

--- startup.m
+++ startup.m
@@ -20,7 +20,7 @@ clear axisColor textColor axesFontSize titleFontSize
 % My misc utilities
 addpath('~/.matlab/utils/')
 % Library from spatial-econometrics.com/
-addpath(genpath('~/.matlab/libs/jplv7/'), '-end')
+addpath(genpath('~/.matlab/libs/'), '-end')
 
 try 
     EditorMacro('Escape', @GiveFocusEditor, 'run');
