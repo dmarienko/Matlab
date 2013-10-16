0891ee8e49eb64416720d0ccd78098c6ef7bcb51
dima <dmitry.ema@gmail.com>
2013-10-16 21:10:06 +0300

    refactoring

--- .matlab/utils/fig.m
+++
@@ -1,9 +0,0 @@
-function g = fig(id, nTitle, varargin)
-    %FIG Custom figure function
-    % 
-    g = figure(id); 
-    set(g, 'Name', nTitle);
-    set(g, 'NumberTitle', 'off');
-    if nargin> 2 && varargin{1}, set(g, 'WindowStyle', 'docked'); end
-end
-
