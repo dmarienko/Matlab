c355c5f3a13522e3ba698ad05bf4a2f0c5275b39
dima <dmitry.ema@gmail.com>
2013-10-20 01:45:52 +0300

    small changes

--- .matlab/libs/quant/tester/retrieveData.m
+++ .matlab/libs/quant/tester/retrieveData.m
@@ -1,12 +1,15 @@
-function S = retrieveData(symbs, from, to)
+function S = retrieveData(symbs, from, to, datasource, frame)
     if ~iscell(symbs), symbs = {symbs}; end
+    
+    if nargin < 4, datasource = 'yahoo.a'; end
+    if nargin < 5, frame = Helper.D; end
     
     % Collect series data
     S = struct('Name',[], 'Tm', [], 'Op', [], 'Hi', [], 'Lo', [], 'Cl', [], 'Vo', []);
     
     k = 1;
     for i = {symbs{:}}
-        d = getData(char(i), from, to);
+        d = getData(char(i), datasource, frame, from, to);
         if ~isempty(d)
             S(k).Name = char(i);
             S(k).Tm = d(:,1);
@@ -20,9 +23,9 @@ function S = retrieveData(symbs, from, to)
     end
 end
 
-function s = getData(symb, from, to)
+function s = getData(symb, datasource, frame, from, to)
     try 
-        s = Helper.fetch('yahoo.a', symb, Helper.D, from, to);
+        s = Helper.fetch(datasource, symb, frame, from, to);
         s = s.toMat(1);
     catch
         s = [];
