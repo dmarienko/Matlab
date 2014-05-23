ffec31e822eff52321b867ae084c83cd9b98f9d9
dima <dmitry.ema@gmail.com>
2014-05-23 14:46:37 +0300

    CHarts colors

---
+++ .Mathematica/Kernel/init.m
@@ -0,0 +1,3 @@
+(** User Mathematica initialization file **)
+SetOptions[{Plot, ListPlot, ListLinePlot, ListLogPlot, LogPlot, LogLinearPlot, ParametricPlot, ParametricPlot3D},  PlotStyle -> ColorData[45, "ColorList"]];
+SetOptions[{Plot, ListPlot, ListLinePlot, Plot3D, ListPlot3D, LogPlot, LogLinearPlot, ListLogPlot, ParametricPlot, ParametricPlot3D},  AxesStyle -> Directive[GrayLevel[0.75], FontSize -> 11, FontFamily -> "Ubuntu Mono"]];
