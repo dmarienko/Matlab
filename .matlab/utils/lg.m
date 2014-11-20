f1dc77ca49f6c9ab5e6269547d7138f07bb6017d
dima <dmitry.ema@gmail.com>
2014-11-21 01:03:27 +0200

    lg imprvmnts

--- .matlab/utils/lg.m
+++ .matlab/utils/lg.m
@@ -1,13 +1,22 @@
 function lg(varargin)
-	fsprog, global iif
+	fsprog, global iif xmap first nth
 	BMFILE = '~/.matlab/bookmarks.mat';
 	if exist(BMFILE, 'file')
 		load(BMFILE, 'bmtable')
 		fprintf(' .----- (''%s'') ---------------\n',pwd());
 		N = numel(bmtable);
-		for i = 1 : N
+		[~,si] = sort({bmtable.mark});
+		k = 1;
+		for i = si
 			name = iif(isempty(bmtable(i).mark), int2str(i), bmtable(i).mark);
-			fprintf(' %s- - - [%s]\t\t%s\n', iif(i==N,'`','+'), name, bmtable(i).path);
+			spaces = 10 - numel(name);
+			[ps, n] = fileparts(bmtable(i).path);
+
+			xx = arrayfun(@(x) nth(cell2mat(x),1), strsplit(ps,'/'),'UniformOutput', false);
+			ps = strjoin(xx(find(cell2mat(xmap(@isempty, xx))==0)), '.');
+
+			fprintf(' %s- - - ''%s''%s%% - %s/%s\n', iif(k==N,'`','+'), name, repmat(' ',[1 spaces]), ps, n);
+			k = k + 1;
 		end
 	end
 end
